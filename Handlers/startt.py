2308e65392fb0413444af3e2ba7fb1b45c69fe48
Call_me <abbishev@mail.ru>
2022-09-14 10:06:06 +0600

    Update

--- Handlers/startt.py
+++ Handlers/startt.py
@@ -30,24 +30,13 @@ async def keyboard(message: types.message):
     await message.answer(f"What day?{back}", reply_markup=inlinekey)
 
 
-async def keyboardd(message: types.message):
-    inlinekey = types.InlineKeyboardMarkup()
-    inlinekey.add(types.InlineKeyboardButton("Monday", callback_data='Monday'))
-    inlinekey.add(types.InlineKeyboardButton("Tuesday", callback_data='Tuesday'))
-    inlinekey.add(types.InlineKeyboardButton("Wednesday", callback_data='Wednesday'))
-    inlinekey.add(types.InlineKeyboardButton("Thursday", callback_data='Thursday'))
-    inlinekey.add(types.InlineKeyboardButton("Friday", callback_data='Friday'))
-
-    await message.answer(f"What day? {back}", reply_markup=inlinekey)
-
-
 async def monday2(callback_query: types.CallbackQuery):
     retk = types.ReplyKeyboardMarkup(resize_keyboard=True)
     back = surrogates.decode("	\u2728")
     b = [f"{back}"]
     retk.add(*b)
     back = surrogates.decode("	\u2728")
-    await bot.answer_callback_query(callback_query.id, text=f"{back} Monday - second week {back}", show_alert=True)
+    await bot.answer_callback_query(callback_query.id, text=f"{back} Monday - second week {back}", show_alert=False)
     await bot.send_message(callback_query.from_user.id, "Средства создания Web-приложений (лек.) Нурпеисова Ж.С., аға оқ. ZOOM at 10:00 - 10:50")
     await bot.send_message(callback_query.from_user.id, "Мультимедиа технологии (лек.) Сарина А.Ж., ст.пр. ZOOM at 12:00 - 12:50")
     await bot.send_message(callback_query.from_user.id, "Технология .NET программирования (лек.) Сарина А.Ж., ст.пр. ZOOM at 1:00 - 1:50")
@@ -60,7 +49,7 @@ async def tuesday2(callback_query: types.CallbackQuery):
     back = surrogates.decode("	\u2728")
     b = [f"{back}"]
     retk.add(*b)
-    await bot.answer_callback_query(callback_query.id, text=f"{back} Tuesday - second week {back}", show_alert=True)
+    await bot.answer_callback_query(callback_query.id, text=f"{back} Tuesday - second week {back}", show_alert=False)
     await bot.send_message(callback_query.from_user.id, "Профессиональный казахский язык (пр.) Кузенбаева А.А., ст.пр. 213/2 at 1:00 - 1:50")
     await bot.send_message(callback_query.from_user.id, "Академическое деловое письмо (лекция) Баяхметова А.А., ас.проф. 106/2	at 2:00 - 2:50", reply_markup=retk)
 
@@ -70,7 +59,7 @@ async def wednesday2(callback_query: types.CallbackQuery):
     back = surrogates.decode("	\u2728")
     b = [f"{back}"]
     retk.add(*b)
-    await bot.answer_callback_query(callback_query.id, text=f"{back} Wednesday - second week {back}", show_alert=True)
+    await bot.answer_callback_query(callback_query.id, text=f"{back} Wednesday - second week {back}", show_alert=False)
     await bot.send_message(callback_query.from_user.id, "Физическая культура at 11:00 - 12:50")
     await bot.send_message(callback_query.from_user.id, "Профессиональный казахский язык (пр.) Кузенбаева А.А., ст.пр. 213/2 at 2:00 - 3:50")
     await bot.send_message(callback_query.from_user.id, "Мультимедиа технологии (пр.) Сарина А.Ж., ст.пр. 313/2 at 4:00 - 4:50", reply_markup=retk)
@@ -81,7 +70,7 @@ async def thursday2(callback_query: types.CallbackQuery):
     back = surrogates.decode("	\u2728")
     b = [f"{back}"]
     retk.add(*b)
-    await bot.answer_callback_query(callback_query.id, text=f"{back} Thursday - second week {back}", show_alert=True)
+    await bot.answer_callback_query(callback_query.id, text=f"{back} Thursday - second week {back}", show_alert=False)
     await bot.send_message(callback_query.from_user.id, "Академическое деловое письмо (пр.) Баяхметова А.А., ас.проф. 307/2 at 2:00 - 2:50")
     await bot.send_message(callback_query.from_user.id, "Технология .NET программирования (лаб.) Сарина А.Ж., ст.пр. 241/2 at 3:00 - 4:50")
     await bot.send_message(callback_query.from_user.id, "Мультимедиа технологии (лаб.) Сарина А.Ж., ст.пр. 241/2 at 5:00 - 6:50", reply_markup=retk)
@@ -92,7 +81,7 @@ async def friday2(callback_query: types.CallbackQuery):
     back = surrogates.decode("	\u2728")
     b = [f"{back}"]
     retk.add(*b)
-    await bot.answer_callback_query(callback_query.id, text=f"{back} Friday - second week {back}", show_alert=True)
+    await bot.answer_callback_query(callback_query.id, text=f"{back} Friday - second week {back}", show_alert=False)
     await bot.send_message(callback_query.from_user.id, "Модели и методы управления (пр.) Кузенбаева А.А., ст.пр. 312/2 at 9:00 - 10:50")
     await bot.send_message(callback_query.from_user.id, "Средства создания Web-приложений (пр.) Нурпеисова Ж.С., аға оқ. 241/2 at 11:00 - 12:50", reply_markup=retk)
 
